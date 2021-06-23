f0fa21ef52e108b6878c3cb4f07675da3b2bcece
Shay Cohen <shay@pentagon2000.com>
2021-06-23 21:44:48 +0300

    Add project files.

---
+++ PairWithSum/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PairWithSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = PairWithSum(5, new List<int>() { 4, 6, 1, 9, -4 });
+
+            // Need to show the result better.
+            Console.WriteLine(result);
+        }
+
+        // Assuming no duplicates in integers.
+        public static List<Tuple<int, int>> PairWithSum(int sum, List<int> nums)
+        {
+            if (nums.Count < 2) return new List<Tuple<int, int>>();
+
+            nums.Sort();
+
+            var result = new List<Tuple<int, int>>();
+            int left = 0;
+            int right = nums.Count - 1;
+
+            while (true)
+            {
+                if (left == right) break;
+                if (left >= nums.Count ||  right < 0) break;
+
+                int candidate = nums[left] + nums[right];
+
+                if (candidate == sum)
+                {
+                    result.Add(new Tuple<int, int>(nums[left], nums[right]));
+                    left++;
+                    right++;
+                }
+
+                if (candidate < sum)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }                
+            }
+
+            return result;
+        }
+    }
+}
